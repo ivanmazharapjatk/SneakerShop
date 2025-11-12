db3e5aeb66f26589ba49f153b83fd55f7bbf3011
IvanMazhara <s31385@pjwstk.edu.pl>
2025-11-12 21:27:29 +0100

    Employee class introduced

---
+++ SneakerShop/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace SneakerShop.Models;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Position { get; set; }
+    public static decimal BaseSalary { get; set; } = 2500m;
+    public bool Bonus { get; set; }
+    public decimal salaryNow { get; set; }
+    public int ClearanceLevel { get; set; }
+}
