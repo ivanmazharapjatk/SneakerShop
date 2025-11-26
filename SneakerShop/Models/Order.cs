3cf44d1b6d0ca6b270f7dc7dd20ac8d5a473b9dc
Mykyta Prokhoda <s31524@pjwstk.edu.pl>
2025-11-26 02:23:19 +0100

    Add Order class

---
+++ SneakerShop/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace SneakerShop.Models;
+
+public class Order
+{
+    public DateTime OrderDate { get; set; }
+    public string PaymentInfo { get; set; }
+    public string Status { get; set; }
+    public decimal TotalAmount { get; set; }
+}
