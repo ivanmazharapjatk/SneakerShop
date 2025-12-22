465c8b2f30d1ca2ff1f765d4641c3bbf2db1225a
Ivan Mazhara <s31385@pjwstk.edu.pl>
2025-12-22 03:07:51 +0100

    Sneaker class restructure

--- SneakerShop/Models/Sneaker.cs
+++ SneakerShop/Models/Sneaker.cs
@@ -9,70 +9,27 @@ namespace SneakerShop.Models;
 
 public class Sneaker : Product
 {
-    private static readonly List<Sneaker> _extent = new();
-    public static IReadOnlyList<Sneaker> Extent => _extent.AsReadOnly();
-    [JsonIgnore]
-    public Brand Brand { get; private set; }
+    #region Constants
     
     private const string ExtentFilePath = "SneakerExtent.json";
     
-    public static void ClearExtent()
-    {
-        foreach (var sneaker in _extent.ToArray())
-        {
-            sneaker.Delete();
-        }
-    }
-
-    public static void SaveExtent()
-    {
-        var options = new JsonSerializerOptions
-        {
-            WriteIndented = true
-        };
-
-        var json = JsonSerializer.Serialize(_extent, options);
-        File.WriteAllText(ExtentFilePath, json);
-    }
-
-    public static void LoadExtent()
-    {
-        if (!File.Exists(ExtentFilePath))
-        {
-            return;
-        }
-
-        ClearExtent();
-
-        var json = File.ReadAllText(ExtentFilePath);
-        
-        _ = JsonSerializer.Deserialize<List<Sneaker>>(json);
-    }
+    #endregion
+    
+    #region Extent Fields
+    
+    private static readonly List<Sneaker> _extent = new();
+    public static IReadOnlyList<Sneaker> Extent => _extent.AsReadOnly();
+    
+    #endregion
+    
+    #region Class Fields
     
     private string _collection;
     private int _size;
-
-    public string Collection
-    {
-        get => _collection;
-        set
-        {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Collection cannot be empty.");
-            _collection = value;
-        }
-    }
-
-    public int Size
-    {
-        get => _size;
-        set
-        {
-            if (value <= 0)
-                throw new ArgumentOutOfRangeException(nameof(Size), "Size must be a positive number.");
-            _size = value;
-        }
-    }
+    
+    #endregion
+    
+    #region Constructors
     
     public Sneaker(
         string name,
@@ -98,7 +55,76 @@ public class Sneaker : Product
 
         _extent.Add(this);
     }
+    
+    #endregion
 
+    #region Attribute Properties and Validation
+    
+    public string Collection
+    {
+        get => _collection;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Collection cannot be empty.");
+            _collection = value;
+        }
+    }
+
+    public int Size
+    {
+        get => _size;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException(nameof(Size), "Size must be a positive number.");
+            _size = value;
+        }
+    }
+    
+    #endregion
+    
+    #region Persistence Logic
+    
+    public static void SaveExtent()
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        var json = JsonSerializer.Serialize(_extent, options);
+        File.WriteAllText(ExtentFilePath, json);
+    }
+
+    public static void LoadExtent()
+    {
+        if (!File.Exists(ExtentFilePath))
+        {
+            return;
+        }
+
+        ClearExtent();
+
+        var json = File.ReadAllText(ExtentFilePath);
+        
+        _ = JsonSerializer.Deserialize<List<Sneaker>>(json);
+    }
+    
+    public static void ClearExtent()
+    {
+        foreach (var sneaker in _extent.ToArray())
+        {
+            sneaker.Delete();
+        }
+    }
+    
+    #endregion
+
+    #region Brand Association
+    
+    [JsonIgnore] public Brand Brand { get; private set; }
+    
     public void AssignBrand(Brand brand)
     {
         if (brand == null) throw new ArgumentNullException(nameof(brand));
@@ -129,4 +155,6 @@ public class Sneaker : Product
         _extent.Remove(this);
         RemoveFromExtent(this);
     }
+    
+    #endregion
 }
