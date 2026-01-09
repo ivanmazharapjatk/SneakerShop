999c309116a968cd0391ed13c21422eb4dfb3f10
Ivan Mazhara <s31385@pjwstk.edu.pl>
2026-01-10 00:05:23 +0100

    Customer class restructure

--- SneakerShop/Models/Customer.cs
+++ SneakerShop/Models/Customer.cs
@@ -4,10 +4,13 @@ namespace SneakerShop.Models
 {
     public class Customer
     {
-        private readonly List<Order> _orderHistory = new();
-        public IReadOnlyList<Order> OrderHistory => _orderHistory.AsReadOnly();
-
+        #region Class Fields
+        
         private string _username = string.Empty;
+        
+        #endregion
+        
+        #region Attribute Validation
         public string Username
         {
             get => _username;
@@ -26,9 +29,18 @@ namespace SneakerShop.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string? PhoneNumber { get; set; }
+        
+        #endregion
+        
+        // Wasn't sure about these two, but they must be separate associations and not actual attributes, so they are in no regions yet.
         public List<Product> Wishlist { get; set; } = new();
         public List<Product> Cart { get; set; } = new();
-
+        
+        #region Order History Association
+        
+        private readonly List<Order> _orderHistory = new();
+        public IReadOnlyList<Order> OrderHistory => _orderHistory.AsReadOnly();
+        
         internal void RegisterOrder(Order order)
         {
             if (order == null) throw new ArgumentNullException(nameof(order));
@@ -42,5 +54,9 @@ namespace SneakerShop.Models
         {
             _orderHistory.Remove(order);
         }
+
+        #endregion
+        
+        //TODO: (!!!) Extent fields and persistence logic accordingly, class constructor, all the data validation, fix the association.
     }
 }
